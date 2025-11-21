117f521a1fb1d939d19263d56d4bef220c1d72a0
KatarzynaPaczos <kat.paczos@gmail.com>
2025-11-21 17:37:10 +0100

    Score calculation impr and unit tests

--- src/score.py
+++ src/score.py
@@ -1,41 +1,43 @@
 import numpy as np
 
 W_CUSTOM = 1.0
-W_STORE  = 5
-W_RATIO = 1
+W_STORE  = 1.0
+W_RATIO = 1.0
 MAX_RADIUS = 1000.0   # cutoff in meters
+EXPECTED_CUST_PER_STORE = 800
 
 
 def customers_proximity(distances, n_residents):
-    return W_CUSTOM * np.sum((1 - (distances/MAX_RADIUS)**(1/3)) * n_residents / 1000) # in K
-
+    ''' In 1km distance - how close is the store to the people'''
+    n_residents = n_residents.reshape(-1)
+    weights = 1 - (distances / MAX_RADIUS) ** (1/3)
+    return W_CUSTOM * np.sum(weights *n_residents) / np.sum(n_residents)
 
 def other_store_proximity(distances):
-    return - W_STORE * np.sum((1 - (distances/MAX_RADIUS))**(1/3))
+    ''' In 1km distance - howclose are the existing stores'''
+    return - W_STORE * np.sum((1 - (distances/MAX_RADIUS))**(1/3))/ len(distances)
 
 
 def ratio_customers_per_store(n_residents, dists_stores):
-    return W_RATIO * np.sum(n_residents)/(len(dists_stores)) # in K
+    customers_per_store = np.sum(n_residents) / len(dists_stores) if len(dists_stores) > 0 else np.sum(n_residents)*2
+    return W_RATIO * np.min([customers_per_store/ EXPECTED_CUST_PER_STORE, 1])
 
 
 def evaluate_fn(x, tree_residents, tree_store, residents_xy, residents_n, stores_xy):
     cust_prox, store_prox, ratio = evaluate_score(x, tree_residents, tree_store, residents_xy, residents_n, stores_xy)
-    return float(cust_prox + store_prox + ratio)
+    return float(1 + cust_prox + store_prox + ratio)
 
 
 def evaluate_score(x, tree_residents, tree_store, residents_xy, residents_n, stores_xy):
     # x is the candidate for the new store. Let's calulate how good is this localisation
     # indexes of all residents in the max_radious around x
-    idx = tree_residents.query_radius(x.reshape(1, -1), r=MAX_RADIUS)[0]
-    dists_residents = np.linalg.norm(residents_xy[idx] - x, axis=1) # distances - vector with
+    idx_res = tree_residents.query_radius(x.reshape(1, -1), r=MAX_RADIUS)[0]
+    dists_residents = np.linalg.norm(residents_xy[idx_res] - x, axis=1) # distances - vector with
 
-    idx = tree_store.query_radius(x.reshape(1, -1), r=MAX_RADIUS)[0]
-    dists_stores = np.linalg.norm(stores_xy[idx] - x, axis=1) # distances - vector with
+    idx_store = tree_store.query_radius(x.reshape(1, -1), r=MAX_RADIUS)[0]
+    dists_stores = np.linalg.norm(stores_xy[idx_store] - x, axis=1) # distances - vector with
 
-    cust_prox = customers_proximity(dists_residents, residents_n[idx])
-    store_prox = other_store_proximity(dists_stores)
-    if len(dists_stores) == 0:
-        ratio = len(residents_n[idx])*2
-    else:
-        ratio = ratio_customers_per_store(residents_n[idx], dists_stores)
+    cust_prox = customers_proximity(dists_residents, residents_n[idx_res]) if len(dists_residents) > 0 else 0.0
+    store_prox = other_store_proximity(dists_stores) if len(dists_stores) > 0 else 0.0
+    ratio = ratio_customers_per_store(residents_n[idx_res], dists_stores)
     return cust_prox, store_prox, ratio
--- tests/test_score.py
+++ tests/test_score.py
@@ -1,39 +1,84 @@
 import numpy as np
 from sklearn.neighbors import KDTree
 
-# tests/test_score.py
-import os
-import sys
-
-print("PYTEST DEBUG cwd:", os.getcwd())
-print("PYTEST DEBUG sys.path:", sys.path)
-
 from src.score import (
     customers_proximity,
     other_store_proximity,
+    ratio_customers_per_store,
     evaluate_score,
+    evaluate_fn,
+    MAX_RADIUS,
 )
 
-def test_customers_proximity_more_customers_higher_score():
-    # same distances, different number of residents
-    d = np.array([100.0, 200.0])
-    few = np.array([10.0, 10.0])
-    many = np.array([50.0, 50.0])
 
-    score_few = customers_proximity(d, few)
-    score_many = customers_proximity(d, many)
+def test_customers_proximity_range_and_monotonic():
+    distances = np.array([100.0, 500.0, 900.0])
+    residents = np.array([10.0, 20.0, 30.0])
 
-    # more people at same distance -> better score
-    assert score_many > score_few
+    val = customers_proximity(distances, residents)
+    assert 0.0 <= val <= 1.0
+
+    # closer = better
+    close = customers_proximity(np.array([100.0]), np.array([100.0]))
+    far = customers_proximity(np.array([900.0]), np.array([100.0]))
+    assert close > far
 
 
-def test_customers_proximity_closer_is_better():
-    # same residents, closer distances should give higher score
-    close = np.array([50.0, 100.0])
-    far = np.array([500.0, 800.0])
-    n_residents = np.array([30.0, 30.0])
+def test_other_store_proximity_range_and_penalty():
+    # two stores close
+    distances_close = np.array([100.0, 200.0])
+    # two stores far
+    distances_far = np.array([800.0, 900.0])
 
-    score_close = customers_proximity(close, n_residents)
-    score_far = customers_proximity(far, n_residents)
+    val_close = -other_store_proximity(distances_close)
+    val_far = -other_store_proximity(distances_far)
 
-    assert score_close > score_far
+    # range 0-1
+    assert 0.0 <= val_close <= 1.0
+    assert 0.0 <= val_far <= 1.0
+    assert val_far < val_close
+
+
+def test_ratio_customers_per_store_range_and_behavior():
+    n_residents = np.array([400.0, 400.0])
+    dists_stores = np.array([100.0])
+    val = ratio_customers_per_store(n_residents, dists_stores)
+    assert 0.0 <= val <= 1.0
+
+    # more customers -> bigger ratio
+    few_customers = np.array([200.0])
+    many_customers = np.array([1600.0])
+
+    val_few = ratio_customers_per_store(few_customers, dists_stores)
+    val_many = ratio_customers_per_store(many_customers, dists_stores)
+
+    assert val_many > val_few
+    assert val_many <= 1.0
+    val_no_stores = ratio_customers_per_store(n_residents, np.array([]))
+    assert val_no_stores == 1.0
+
+
+def test_evaluate_score_components_and_total_range():
+    # simple: 2 buildings, 1 store far away, 1 store close
+    residents_xy = np.array([[0.0, 0.0], [500.0, 0.0]])
+    residents_n = np.array([100.0, 100.0])
+    stores_xy = np.array([[2000.0, 0.0]])
+    tree_residents = KDTree(residents_xy)
+    tree_stores = KDTree(stores_xy)
+
+    x = np.array([100.0, 0.0])  # candidate location
+
+    cust_prox, store_prox, ratio = evaluate_score(
+        x, tree_residents, tree_stores, residents_xy, residents_n, stores_xy
+    )
+
+    assert 0.0 <= cust_prox <= 1.0
+    assert 0.0 <= store_prox <= 1.0
+    assert 0.0 <= ratio <= 1.0
+
+    total = 1 + cust_prox + store_prox + ratio
+    assert 0.0 <= total <= 3.0
+
+    # evaluate_fn should give the same total
+    fn_val = evaluate_fn(x, tree_residents, tree_stores, residents_xy, residents_n, stores_xy)
+    assert np.isclose(fn_val, total)
